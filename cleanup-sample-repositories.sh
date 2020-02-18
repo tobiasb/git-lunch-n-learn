c4309ddaf3aafae18ee38df1d48735cff1af3c15
Tobias Boehm <tobias.boehm@boeing.com>
2020-02-18 11:06:53 -0800

    Improve cleanup script

--- cleanup-sample-repositories.sh
+++ cleanup-sample-repositories.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cd repos/secret-santa-sms-a
-git reset version-1.0 --hard
-git push -f
+if [[ -d "repos/secret-santa-sms-a" ]]; then
+    cd repos/secret-santa-sms-a
+    git checkout master && git reset version-1.0 --hard && git push -f
+    git push origin --delete my-branch
+    git push origin --delete improve-tests
+    cd ../..
+fi
+
+rm -rf repos/
